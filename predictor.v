4f288fb0d82cd573506d18bdd8f90ccc33c8b992
pariyahr <pariyahajipour@gmail.com>
2023-06-01 10:36:50 +0330

    daf

--- predictor.v
+++ predictor.v
@@ -1,30 +1,38 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-// Your code
-parameter n = 4;
-parameter m = 1 << n;
+// // Your code
+// parameter n = 4;
+// parameter m = 1 << n;
 
-reg [n - 1:0] last_results;
+// reg [n - 1:0] last_results;
 
-reg [1:0] array[m - 1:0];
+// reg [1:0] array[m - 1:0];
 
-reg [1:0]counter;
+reg [1:0]counter = 3;
+reg [1:0] temp = 0;
 
 always @(posedge clk) begin
     if (request) begin
-      counter = array[last_results];
+    //   counter = array[last_results];
       if (counter[1]) prediction = 1;
       else prediction = 0;
     end else if (result) begin
-        counter = array[last_results];
+        // counter = array[last_results];
+        // if (taken) begin
+        //     if (counter < 3) array[last_results] = array[last_results] + 1;
+        // end else begin
+        //     if (counter > 0) array[last_results] = array[last_results] - 1;
+        // end
+        temp = counter;
         if (taken) begin
-            if (counter < 3) array[last_results] = array[last_results] + 1;
+            if (counter < 3) temp = temp + 1;
         end else begin
-            if (counter > 0) array[last_results] = array[last_results] - 1;
+            if (counter > 0) temp = temp - 1;
         end
+        counter = temp;
 
-        last_results = last_results << 1;
-        last_results[n - 1] = taken;
+        // last_results = last_results << 1;
+        // last_results[n - 1] = taken;
     end
 
 end
