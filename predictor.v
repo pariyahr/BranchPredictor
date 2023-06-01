c1e53469f1a57dc13aeb0639015177d59afc826e
pariyahr <pariyahajipour@gmail.com>
2023-06-01 10:30:04 +0330

    adaf

--- predictor.v
+++ predictor.v
@@ -8,27 +8,25 @@ reg [n - 1:0] last_results;
 
 reg [1:0] array[m - 1:0];
 
+reg [1:0]counter;
 
 always @(posedge clk) begin
     if (request) begin
-      reg counter = array[last_results];
+      counter = array[last_results];
       if (counter[1]) prediction = 1;
       else prediction = 0;
     end else if (result) begin
-        reg counter = array[last_results];
+        counter = array[last_results];
         if (taken) begin
-            if (counter < 3) array[last_results] += 1;
+            if (counter < 3) array[last_results] = array[last_results] + 1;
         end else begin
-            if (counter > 0) array[last_results] -= 1;
+            if (counter > 0) array[last_results] = array[last_results] - 1;
         end
 
         last_results = last_results << 1;
         last_results[n - 1] = taken;
     end
 
-
 end
-
-
 
 endmodule
